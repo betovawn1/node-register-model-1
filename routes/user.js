a9cb74cde8108d14d042dd95e871c4c861545a81
Betovawn <sergioluizfrancajr@gmail.com>
2020-03-29 00:45:01 -0300

    Usando criptografia para senha

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../model/user')
+const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
     user.find({}, (err, data) => {
@@ -23,6 +24,23 @@ router.post('/create', (req, res) => {
             return res.send({error: 'Usuário cadastrado => ', data})
         })
     })
+})
+
+router.post('/auth', (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) return res.send({error: 'Dados Insuficientes'})
+
+    user.findOne({email}, (err, data) => {
+        if (err) return res.send({error: 'Erro ao buscar usuário!'})
+        if (!data) return res.send({error: 'Usuário não existe'})
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.send({error: 'Erro ao autenticar usuário'})
+
+            return res.send(data)
+        })
+    }).select('+password')
 })
 
 module.exports = router;
