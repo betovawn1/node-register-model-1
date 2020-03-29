bc6b4e96423dd7fa25de041541af08028ddde5d4
Betovawn <sergioluizfrancajr@gmail.com>
2020-03-29 01:19:06 -0300

    refatoração de endpoints para funções assíncronas

--- routes/user.js
+++ routes/user.js
@@ -3,44 +3,45 @@ const router = express.Router()
 const user = require('../model/user')
 const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-    user.find({}, (err, data) => {
-        if (err) return res.send({error: 'Erro na consulta de usuários!'})
+router.get('/', async (req, res) => {
+    try {
+        const data = await user.find({})
         return res.send(data)
-    })
+    } catch (err) {
+        return res.send({error: 'Erro na consulta de usuários!'})
+    }
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const {email, password} = req.body
-
     if (!email || !password) return res.send({error: 'Dados Insuficientes'})
 
-    user.findOne({email}, (err, data) => {
-        if (err) return res.send({error: 'Erro ao buscar usuário!'})
-        if (data) return res.send({error: 'Usuário já cadastrado'})
+    try {
+        if (await user.findOne({email})) return res.send({error: 'Usuário já cadastrado'}) 
 
-        user.create(req.body, (err, data) => {
-            if (err) return res.send({error: 'Erro ao cadastrar usuário!'})
-            return res.send({error: 'Usuário cadastrado => ', data})
-        })
-    })
+        const data = await user.create(req.body)
+        return res.send(data)
+    } catch (err) {
+        return res.send({error: 'Erro ao buscar usuário!'})
+    }
 })
 
-router.post('/auth', (req, res) => {
+router.post('/auth', async (req, res) => {
     const {email, password} = req.body;
-
     if (!email || !password) return res.send({error: 'Dados Insuficientes'})
 
-    user.findOne({email}, (err, data) => {
-        if (err) return res.send({error: 'Erro ao buscar usuário!'})
-        if (!data) return res.send({error: 'Usuário não existe'})
+    try {
+        const data = await user.findOne({email}).select('+password')
+        if (!data) res.send({error: 'Usuário não registrado'})
 
-        bcrypt.compare(password, data.password, (err, same) => {
-            if (!same) return res.send({error: 'Erro ao autenticar usuário'})
+        const pass_ok = await bcrypt.compare(password, data.password)
+        if (!pass_ok) res.send({error: 'Erro na comparação de senhas'})
+        
+        return res.send(data)
 
-            return res.send(data)
-        })
-    }).select('+password')
+    } catch (err) {
+        return res.send({error: 'Erro ao buscar usuário!'})
+    }
 })
 
 module.exports = router;
